bf45ed1de4f5a5ee75d7520f54bebf19c0471377
Matheus Quost <matheusquost@gmail.com>
2020-09-01 23:35:08 -0300

    Anúncios Magalu Adicionados!

--- js/main.js
+++ js/main.js
@@ -1,3 +1,12 @@
+(function(storeName) {
+    var b = document.createElement('script');
+    b.type = 'text/javascript';
+    b.async = true;
+    b.src = 'https://www.magazinevoce.com.br/js/banner.js?store=' + storeName;
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(b, s);
+})('matheusquost');
+
 AOS.init({
     duration: 800,
     easing: 'ease',
